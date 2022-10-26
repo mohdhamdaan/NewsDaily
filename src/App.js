0c4abd891c0decc6ded491b216fccc94b5c046c8
mohdhamdaan <111704566+mohdhamdaan@users.noreply.github.com>
2022-10-26 12:20:38 +0530

    newsweb app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { useState } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar';
 
-export default App;
+const App = ()=> {
+
+ const[progress, setProgress] = useState(0)
+//  apikey= "54e266f93c04463baf69e966ad1c3b00"
+
+  
+    return (
+      
+      <div>
+        <Router>
+        <Navbar/>
+
+        <LoadingBar
+        height = {3}
+        color='#f11946'
+        progress={progress}
+        // onLoaderFinished={() => setProgress(0)}
+      />
+        
+        <Routes>
+          <Route exact path="/general" element = {<News setProgress={ setProgress}   key="general" pageSize={6} category="general" country = "in"/>}></Route>
+          <Route exact path="/" element = {<News setProgress={ setProgress}   key="general" pageSize={6} category="general" country = "in"/>}></Route>
+          <Route exact path="/business" element = {<News setProgress={ setProgress}   key="business" pageSize={6} category="business" country = "in"/>}></Route>
+          <Route exact path="/entertainment" element = {<News setProgress={ setProgress}   key="entertainment" pageSize={6} category="entertainment" country = "in"/>}></Route>
+          <Route exact path="/health" element = {<News setProgress={ setProgress}   key="health" pageSize={6} category="health" country = "in"/>}></Route>
+          <Route exact path="/science" element = {<News setProgress={ setProgress}   key="science" pageSize={6} category="science" country = "in"/>}></Route>
+          <Route exact path="/technology" element = {<News setProgress={ setProgress}   key="technology" pageSize={6} category="technology" country = "in"/>}></Route>
+          <Route exact path="/sports" element = {<News setProgress={ setProgress}   key="sports" pageSize={6} category="sports" country = "in"/>}></Route>
+         
+        </Routes>
+        </Router>
+      </div>
+      
+    )
+  
+}
+export default App
---
+++ src/components/NewsItem.js
@@ -0,0 +1,38 @@
+
+import React from 'react'
+
+const NewsItem =(props)=> {
+
+    
+        let { description, newsUrl, imageUrl, title, author, date , source} = props
+        return (
+            <div>
+                <div className="card" >
+                    <div style={
+                        {
+                            display:'flex',
+                            justifyContent: 'flex-end',
+                            position: 'absolute',
+                            right: '0'
+
+                        }
+                    }>
+                <span className=" badge rounded-pill bg-danger" >
+                            {source}
+                        </span>
+                        </div>
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        {/* <p className="card-text">{description}</p> */}
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text">By {author} on {new Date(date).toGMTString()}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+
+}
+
+export default NewsItem
